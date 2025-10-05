4ab9f80348990b8559eefe2fcdd7e39495970174
Dimitris Alo <47322687+dimitris4000@users.noreply.github.com>
2025-10-05 22:28:37 +0300

    Update example role resource.tf
    Update resource role example with an owner example

--- examples/resources/redshift_role/resource.tf
+++ examples/resources/redshift_role/resource.tf
@@ -1,8 +1,13 @@
 resource "redshift_role" "role" {
-  name      = "RoleName"
+  name = "RoleName"
 }
 
 resource "redshift_role" "role" {
   name        = "RoleName"
   external_id = "ABC123"
+}
+
+resource "redshift_role" "role" {
+  name  = "RoleName"
+  owner = "OwnerUserName"
 }
